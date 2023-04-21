18fcc613fff545166dc03aa9c8c7273a18098d02
MiguelAngelLardo <miguel.lardo@yahoo.com.ar>
2023-04-21 12:21:27 -0300

    Clase LectorDeArchivo creada

--- Tp_Final_Prode/src/main/java/Main.java
+++ Tp_Final_Prode/src/main/java/Main.java
@@ -6,26 +6,12 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IOException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/Equipos?useSSL=false&allowPublicKeyRetrieval=true", "root", "root");
-        Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("select * from Partidos");
+        Campeonato campeonatoActual = LectorDeArchivo.leerPartidosDesdeBD();
+        Prode prodeActual = LectorDeArchivo.leerApuestasDesdeDB(campeonatoActual);
 
-        Campeonato campeonatoActual = new Campeonato(); //a este Array le agrego mas partidos
-
-        while (rs.next()){
-            String equipoLocal = rs.getString("equipolocal");
-            int golesLocal = rs.getInt("goleslocal");
-            int golesVisitante = rs.getInt("golesvisitante");
-            String equipoVisitante = rs.getString("equipovisitante");
-            String ronda = rs.getString("ronda");
-
-            Partido nuevo = new Partido(equipoLocal, equipoVisitante, golesLocal, golesVisitante, ronda);
-            campeonatoActual.agregarPartido(nuevo);
-
-        }
-        conn.close(); rs.close();
-
-        /*Scanner sc = new Scanner(System.in);
+        //Leer desde Archivo
+        /*
+        Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese el archivo con los partidos: ");
         String a = sc.nextLine();
         Path archivo = Paths.get(a);
@@ -42,27 +28,11 @@ public class Main {
             Partido nuevo = new Partido(equipoLocal, equipoVisitante, golesLocal, golesVisitante, ronda);
             campeonatoActual.agregarPartido(nuevo);
         }
-        lector.close();*/
+        lector.close();
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////
 
-        Connection conn2 = DriverManager.getConnection("jdbc:mysql://localhost/Equipos?useSSL=false&allowPublicKeyRetrieval=true", "root", "root");
-        Statement st2 = conn2.createStatement();
-        ResultSet rs2 = st2.executeQuery("select * from Apuestas");
-
-        Prode prodeActual = new Prode(campeonatoActual); //aca uno las 2 listas.
-
-        while (rs2.next()){
-            String nombre = rs2.getString("nombre");
-            String equipoLocalApuesta = rs2.getString("equipolocal");
-            String equipoVisitanteApuesta = rs2.getString("equipovisitante");
-            int resul = rs2.getInt("resultado");
-            String rondaApuesta = rs2.getString("ronda");
-            Apuesta nueva = new Apuesta(nombre, equipoLocalApuesta, equipoVisitanteApuesta,rondaApuesta, resul);
-            prodeActual.agregarApuesta(nueva);
-        }
-        conn2.close(); rs2.close();
-
-        /*System.out.print("Ingrese el archivo con las apuestas: ");
+        System.out.print("Ingrese el archivo con las apuestas: ");
         String b = sc.nextLine();
         Path archivo2 = Paths.get(b);
         Scanner lector2 = new Scanner(archivo2);
@@ -80,8 +50,6 @@ public class Main {
         }
         lector2.close();*/
 
-
-
         System.out.println(campeonatoActual);//muestra el toString de Campeonato (que tiene el de Partido)
         String resultado = prodeActual.apuestasCorrectasPorParticipante();
         System.out.println(resultado);
@@ -94,8 +62,6 @@ public class Main {
         ObjectMapper om2 = new ObjectMapper();
         //System.out.println(om2.writerWithDefaultPrettyPrinter().writeValueAsString(prodeActual));
         //om.writerWithDefaultPrettyPrinter().writeValue(Paths.get("apuestas.json").toFile(), prodeActual);
-
-
 
 
     }
